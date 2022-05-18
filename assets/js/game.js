3f5bd3e85b9cc28e9724156a3e800bedda2fd8c7
kodapassey <kodapassey@charter.net>
2022-05-18 14:51:09 -0700

    added initial JS for gladiator game

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,10 @@
-window.alert("this is an alert! JavaScript is running!");
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a message");
+console.log(10 + 20);
+console.log("Our robots name is " + playerName);
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+// fight();
