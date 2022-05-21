5f42507202b8fcb69caf8b04225cb1aad08634ff
kodapassey <kodapassey@charter.net>
2022-05-21 09:05:54 -0700

    object optimization

--- assets/js/game.js
+++ assets/js/game.js
@@ -21,16 +21,19 @@ var fight = function (enemyName) {
 
             // if yes (true), leave fight
             if (confirmSkip) {
-                window.alert("playerName" + " has decided to skip this fight. Goodbye!");
+                window.alert(playerName + " has decided to skip this fight. Goodbye!");
                 // subtract money from playerMoney for skipping
-                playerMoney = playerMoney - 10;
+                playerMoney = Math.max(0, playerMoney - 10);
                 console.log("playerMoney", playerMoney);
                 break;
             }
         }
 
-        // remove enemy's health by subtracting the amount set in the playerAttack variable
-        enemyHealth = enemyHealth - playerAttack;
+        // generate random damage value based on player's attack power
+        var damage = randomNumber(playerAttack - 3, playerAttack);
+
+        enemyHealth = Math.max(0, enemyHealth - damage);
+
         console.log(
             playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
         );
@@ -38,7 +41,6 @@ var fight = function (enemyName) {
         // check enemy's health
         if (enemyHealth <= 0) {
             window.alert(enemyName + " has died!");
-
 
             // award player money for winning
             playerMoney = playerMoney + 20;
@@ -49,8 +51,9 @@ var fight = function (enemyName) {
         }
 
 
-        // remove player's health by subtracting the amount set in the enemyAttack variable
-        playerHealth = playerHealth - enemyAttack;
+        var damage = randomNumber(enemyAttack - 3, enemyAttack);
+
+        playerHealth = Math.max(0, playerHealth - damage);
         console.log(
             enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
         );
@@ -67,6 +70,13 @@ var fight = function (enemyName) {
     } // while loops closing tag
 }; // end of fight function
 
+
+// function to generate a random numeric value
+var randomNumber = function (min, max) {
+    var value = Math.floor(Math.random() * (max - min + 1) + min);
+
+    return value;
+}
 
 // function to start new game
 var startGame = function () {
@@ -86,8 +96,7 @@ var startGame = function () {
             var pickedEnemyName = enemyNames[i];
 
             // reset enemyHealth before starting new fight
-            enemyHealth = 50;
-
+            enemyHealth = randomNumber(40, 60);
             // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
             fight(pickedEnemyName);
 
@@ -101,14 +110,14 @@ var startGame = function () {
                     shop();
                 }
             }
-
-            // if player isn't alive, stop the game
-            else {
-                window.alert("You have lost your robot in battle! Game Over!");
-                break;
-            }
         }
+        // if player isn't alive, stop the game
+        else {
+            window.alert("You have lost your robot in battle! Game Over!");
+            break;
+        };
     }
+
 
     // after the loop ends, player is either out of health or enemies to fight, so run the endGame function
     endGame();
@@ -124,7 +133,7 @@ var endGame = function () {
         window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
     } else {
         window.alert("You've lost your robot in battle.");
-    }
+    };
 
     // ask player if they'd like to play again
     var playAgainConfirm = window.confirm("Would you like to play again?");
